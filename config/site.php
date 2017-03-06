dde3aa822a990481f822a791e7f4ea3ce0d5f878
Uriah Galang <g0ld3lux@gmail.com>
2017-03-06 14:57:32 +0800

    change site config

--- config/site.php
+++ config/site.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-  'title' => env('SITE_TITLE', 'Jarvis Bot Messenger'),
+  'title' => env('SITE_TITLE', 'BotRoo - Facebook Bot Messenger Builder'),
   'url' => env('APP_URL', 'http://localhost'),
   'logo' => env('SITE_LOGO', 'http://localhost/logo.png'),
-  'brand' => env('BRAND', 'Jarvis')
+  'brand' => env('BRAND', 'BotRoo')
 ];
