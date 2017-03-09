17107033e72dbb65d360ce9d7eec4e495796a94f
Uriah Galang <g0ld3lux@gmail.com>
2017-03-10 01:13:49 +0800

    install lazychaser categories , and rtconner/laravel-tagging, plus create a templates migration table

---
+++ database/migrations/2017_03_09_162724_create_templates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('category_id')->nullable();
+            $table->integer('tag_id')->nullable();
+            $table->longText('flows');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('templates');
+    }
+}
---
+++ database/migrations/2017_03_09_165642_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug')
+                  ->unique();
+            // NestedSet::columns($table);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('categories', function (Blueprint $table) {
+        // NestedSet::dropColumns($table);
+        // });
+        Schema::drop('categories');
+    }
+}
